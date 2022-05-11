1fd5a281332a8853cb8e08133a53a18ee3e76530
Bruno Gomes <brunog.vix@gmail.com>
2022-05-10 22:03:40 -0300

    creating the hotel controller

---
+++ README.md
@@ -0,0 +1,8 @@
+# Apartment and Hotel Booking
+
+## MERN project using MongoDB, Express, React and NodeJS
+
+# About the Project
+This is a website that you can find apartments and hotels that have rooms available to sleep, you can select how many days you want to use and the price that you'll pay for it.
+
+You have the Admin part that you can create AD in the system and other users can check it out!
---
+++ api/routes/hotels.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel_controller.js";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+
+const router = express.Router();
+
+// CREATE
+router.post("/", createHotel);
+
+// UPDATE
+router.put("/:id", updateHotel);
+
+// DELETE
+router.delete("/:id", deleteHotel);
+
+// GET
+router.get("/:id", getHotel);
+// GET ALL
+router.get("/", getHotels);
+
+export default router;
